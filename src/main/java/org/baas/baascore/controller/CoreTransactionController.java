f079f71fe9328c72507ecc6001d595bd806ee4ad
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-11-10 17:44:17 +0900

    feat : 계좌이체 기능 구

--- src/main/java/org/baas/baascore/controller/CoreTransactionController.java
+++ src/main/java/org/baas/baascore/controller/CoreTransactionController.java
@@ -1,7 +1,11 @@
 package org.baas.baascore.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.baas.baascore.dto.TransferRequestDto;
+import org.baas.baascore.dto.TransferResponseDto;
 import org.baas.baascore.service.CoreTransactionService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/transfer")
 public class CoreTransactionController {
     private final CoreTransactionService coreTransactionService;
+
+    @PostMapping
+    public TransferResponseDto withdraw(@RequestBody TransferRequestDto transferRequestDto) {
+        return coreTransactionService.transfer(transferRequestDto);
+    }
 }
