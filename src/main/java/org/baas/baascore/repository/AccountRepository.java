ef19bc74ef6971c8c2c0791df14d076fbd2c57de
dlaudfuf33 <dlaudfuf33@gmail.com>
2024-11-10 17:42:32 +0900

    refact : 단순조회 , 수정위한(비관락) 조회 분리

--- src/main/java/org/baas/baascore/repository/AccountRepository.java
+++ src/main/java/org/baas/baascore/repository/AccountRepository.java
@@ -1,9 +1,14 @@
 package org.baas.baascore.repository;
 
+import jakarta.persistence.LockModeType;
 import org.baas.baascore.model.Account;
 import org.baas.baascore.model.Customer;
 import org.baas.baascore.util.AccountType;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +16,24 @@ import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    // 단순 조회 (락 없이)
     Optional<Account> findByAccountNumber(String accountNumber);
+
     Optional<Account> findByFintechUseNum(String fintechUseNum);
+
     List<Account> findByCustomerAndIsDeletedAndAccountType(Customer customer, boolean isDeleted, AccountType accountType);
+
+    // 수정 작업을 위한 조회 (JPQL + 락 적용)
+    @Query("SELECT a FROM Account a WHERE a.accountNumber = :accountNumber")
+    @EntityGraph(attributePaths = {"customer", "bank"})
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Account> findByAccountNumberForUpdate(@Param("accountNumber") String accountNumber);
+
+    @Query("SELECT a FROM Account a WHERE a.fintechUseNum = :fintechUseNum")
+    @EntityGraph(attributePaths = {"customer", "bank"})
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Account> findByFintechUseNumForUpdate(@Param("fintechUseNum") String fintechUseNum);
+
+
 }
