be1e40a34c36f970d6583157574fc134a4bf7318
dlaudfuf33 <dlaudfuf33@gmail.com>
2025-01-12 19:47:32 +0900

    refact : Transaction SUCCESS 필터링

--- src/main/java/org/baas/baascore/repository/TransactionHistoryRepository.java
+++ src/main/java/org/baas/baascore/repository/TransactionHistoryRepository.java
@@ -14,15 +14,32 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory,Long> {
+public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Long> {
     Optional<TransactionHistory> findByCoreTransactionIdAndAccount_FintechUseNum(Long id, String fintechNum);
 
-    @Query("select t from TransactionHistory t where t.account =:account and t.createdAt >= :filteredDate order by t.createdAt ASC")
+    @Query("""
+            select t 
+            from TransactionHistory t 
+            join t.coreTransaction ct 
+            where t.account = :account 
+              and t.createdAt >= :filteredDate 
+              and ct.status = org.baas.baascore.util.StatusType.SUCCESS 
+            order by t.createdAt ASC
+            """)
     @EntityGraph(attributePaths = {"card", "account"})
     List<TransactionHistory> findTransactionHistoryAllTranType(@Param("account") Account account,
                                                                @Param("filteredDate") LocalDateTime filteredDate);
 
-    @Query("select t from TransactionHistory t where t.account =:account and t.createdAt >= :filteredDate and t.tranType = :transactionType order by t.createdAt ASC")
+    @Query("""
+            select t 
+            from TransactionHistory t 
+            join t.coreTransaction ct 
+            where t.account = :account 
+              and t.createdAt >= :filteredDate 
+              and t.tranType = :transactionType 
+              and ct.status = org.baas.baascore.util.StatusType.SUCCESS 
+            order by t.createdAt ASC
+            """)
     @EntityGraph(attributePaths = {"card", "account"})
     List<TransactionHistory> findTransactionHistorySelectedTranType(@Param("account") Account account,
                                                                     @Param("filteredDate") LocalDateTime filteredDate,
@@ -31,8 +48,8 @@ public interface TransactionHistoryRepository extends JpaRepository<TransactionH
 
     @Query("SELECT t FROM TransactionHistory t WHERE t.account =:account AND FUNCTION('YEAR', t.createdAt) = :year AND FUNCTION('MONTH', t.createdAt) = :month AND t.tranType = :transactionType ORDER BY t.createdAt DESC")
     List<TransactionHistory> findTransactionHistoryYearAndMonth(@Param("account") Account account,
-        @Param("year") Integer year,
-        @Param("month") Integer month,
-        @Param("transactionType") TranType transactionType);
+                                                                @Param("year") Integer year,
+                                                                @Param("month") Integer month,
+                                                                @Param("transactionType") TranType transactionType);
 
 }
